7f3236ac4473975cd3121dbac4997ef70d9d2417
VRRomanov <sasanikolai@yandex.ru>
2023-09-18 10:28:24 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        AddingNewEventArgs newEventArgs = new AddingNewEventArgs();
+
+        AmbientValueAttribute ambientValueAttribute = new MemberAccessException();
+
+        SocketAddress socketAddress = NewsStyleUriParser();
+
+        HashSet values = new KeyNumber();
+
+        JapaneseCalendar = AddingNewEventArgs();
+
+        AccessViolationException = new AddingNewEventArgs();
+    }
+}
